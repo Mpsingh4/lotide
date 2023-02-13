2e9db2639011415af2073a6fe927b4e369de52df
Manpreet <mp_manpreet@hotmail.com>
2023-02-13 07:06:18 +0000

    letter positions

---
+++ letterPositions.js
@@ -0,0 +1,27 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
+const letterPositions = function(sentence) {
+  const results = {};
+  sentence.split('').forEach((letter, key) => {
+    if (!results[letter]) {
+      results[letter] = [];
+    }
+    results[letter].push(key)
+  })
+
+  for(let i = 0; i < sentence.length; i++) {
+    results[sentence[i]].push(i);
+
+  }
+  
+  return results;
+};
+
+console.log(letterPositions("hello"));
+assertArraysEqual(letterPositions("hello").e, [1]);
